4090fb37393e4e3812538d7f139d086c91d07c3f
Alaa Khoury <alaa.khoury@houzz.com>
2020-08-06 02:42:07 +0300

    .

--- iPayment/LoginScreen/LoginView.swift
+++ iPayment/LoginScreen/LoginView.swift
@@ -12,35 +12,43 @@ struct LoginView: View {
     @State var email: String = ""
     @State var password: String = ""
     
+    private var viewModel = LoginViewModel()
+    
     var body: some View {
-        VStack(alignment: .center) {
-            Text("Login")
-                .configAsTitle()
-                .padding(.bottom, 32)
-                
-            Group {
-                TextField("Email", text: $email)
-                    .modifier(DefaultTextField())
-                    .keyboardType(.emailAddress)
-                
-                SecureField("Password", text: $password)
-                    .modifier(DefaultSecureField())
-            }
-            
-            Button(action: {
-                
-            }, label: {
+        NavigationView {
+            VStack(alignment: .center) {
                 Text("Login")
-                })
-            .modifier(DefaultButton())
-            
+                    .configAsTitle()
+                    .padding(.bottom, 32)
+                    
+                Group {
+                    TextField("Email", text: $email)
+                        .modifier(DefaultTextField())
+                        .keyboardType(.emailAddress)
+                    
+                    SecureField("Password", text: $password)
+                        .modifier(DefaultSecureField())
+                }
+                
+                Button(action: {
+                    self.login()
+                }, label: {
+                    Text("Login")
+                    })
+                .modifier(DefaultButton())
+                
+            }
+            .frame(width: 300)
+            .padding(.vertical, 32.0)
+            .padding(.horizontal, 16.0)
+            .cornerRadius(5)
+            .border(Colors.darkBlue, width: 2)
+            .cornerRadius(5)
         }
-        .frame(width: 300)
-        .padding(.vertical, 32.0)
-        .padding(.horizontal, 16.0)
-        .cornerRadius(5)
-        .border(Colors.darkBlue, width: 2)
-        .cornerRadius(5)
+    }
+    
+    func login() {
+        viewModel.performLogin(self.email, self.password)
     }
 }
 
---
+++ iPayment/LoginScreen/LoginViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewModel.swift
+//  iPayment
+//
+//  Created by Alaa Khoury on 06/08/2020.
+//  Copyright © 2020 Alaa Khoury. All rights reserved.
+//
+
+import SwiftUI
+
+class LoginViewModel {
+    
+    
+    func performLogin(_ username: String, _ password: String) {
+        
+    }
+    
+}
